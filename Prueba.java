18f8eb37c3244c36cf7706209683d1cb47879fcb
Silvi <silviadelucasalvarez@gmail.com>
2022-09-28 11:16:35 +0200

    Muestra el artista favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Apellidos: De Lucas Alvarez");
 		System.out.println("Edad: 19 Año de nacimiento: 2003");
 		System.out.println("Comida favorita: Macarrones");
+		System.out.println("Grupo de música o cantante favorito: Rosalia ");
 
 	}
 	
